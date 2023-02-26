c8d4dced294e3c28dd59f5c4f4a222771bcf4859
0IO11O <125455569+0IO11O@users.noreply.github.com>
2023-02-26 20:23:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Index for HOME
+document.getElementById("home").addEventListener("click", function() {
+  window.location.href = "index.html";
+});
+
+// Index for IMAGES
+document.getElementById("images").addEventListener("click", function() {
+  window.location.href = "images.html";
+});
