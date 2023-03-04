5ecca30a55e5366fd752def3315dec543a0e5f97
0IO11O <125455569+0IO11O@users.noreply.github.com>
2023-03-04 21:00:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Index for HOME
-document.getElementById("home").addEventListener("click", function() {
-  window.location.href = "bilgi.html";
-});
-
-// Index for IMAGES
-document.getElementById("images").addEventListener("click", function() {
-  window.location.href = "images.html";
-});
