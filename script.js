1f9087075419a1e7498f8e72869d81e045ecc968
0IO11O <125455569+0IO11O@users.noreply.github.com>
2023-03-04 21:00:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+	var logo = document.getElementById("logo");
+	var title = document.getElementById("title");
+	var buttons = document.getElementById("buttons");
+
+	logo.style.display = "block";
+	setTimeout(function() {
+		logo.style.opacity = "1";
+	}, 500);
+
+	setTimeout(function() {
+		title.classList.remove("hidden");
+	}, 2000);
+
+	setTimeout(function() {
+		buttons.classList.remove("hidden");
+	}, 2500);
+}
